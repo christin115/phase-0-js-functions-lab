60e39ea27e7cd9c1af02d769e95135f5eddd2f55
Christin115 <cmjordan115@gmail.com>
2026-05-08 20:58:29 -0400

    passed tests

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+
+function isPalindrome(word) {
+    let reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * discountPercentage / 100);
+}
+
+console.log(calculateTax(200)); 
+console.log(convertToUpperCase("hello")); 
+console.log(findMaximum(10, 25)); 
+console.log(isPalindrome("radar")); 
+console.log(isPalindrome("hello")); 
+console.log(calculateDiscountedPrice(100, 20)); 
 
 
 
